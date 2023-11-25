f514d1039eb64a9a74cbdb0f4da9cbfe9db59b65
Your Name <you@example.com>
2023-11-25 12:48:34 +0100

    Setup router

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { EvidenceCollectionProvider } from './context/EvidenceCollectionProvider.tsx';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 const root: HTMLElement | null = document.getElementById('root');
@@ -9,8 +10,10 @@ const root: HTMLElement | null = document.getElementById('root');
 root !== null &&
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <EvidenceCollectionProvider>
-        <App />
-      </EvidenceCollectionProvider>
+      <BrowserRouter>
+        <EvidenceCollectionProvider>
+          <App />
+        </EvidenceCollectionProvider>
+      </BrowserRouter>
     </React.StrictMode>,
   );
