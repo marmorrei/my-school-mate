6d4366301d91436cb8e1d824cabf1ab3567567a8
Your Name <you@example.com>
2023-12-01 11:07:40 +0100

    fix duplication

--- src/components/DesktopTeacherTabs/DesktopTeacherTabs.tsx
+++ src/components/DesktopTeacherTabs/DesktopTeacherTabs.tsx
@@ -1,35 +1,53 @@
 import { NavLink } from 'react-router-dom';
 
+const teacherTabsInfo = [
+  {
+    path: '/dashboard',
+    tabName: 'Dashboard',
+  },
+  {
+    path: '/students',
+    tabName: 'Students',
+  },
+  {
+    path: '/calendar',
+    tabName: 'Timetable/Calendar',
+  },
+  {
+    path: '/learningsituations',
+    tabName: 'Learning situations',
+  },
+  {
+    path: '/evaluation',
+    tabName: 'Evaluation',
+  },
+  {
+    path: '/communication',
+    tabName: 'Comunication',
+  },
+  {
+    path: '/documentation',
+    tabName: 'Documentation',
+  },
+  {
+    path: '/settings',
+    tabName: 'Settings',
+  },
+];
+
+export const teacherTabs = teacherTabsInfo.map(tab => (
+  <li key={tab.tabName}>
+    <NavLink to={tab.path}>{tab.tabName}</NavLink>
+  </li>
+));
+
 const DesktopTeacherTabs = (): JSX.Element => {
   return (
     <ul
       className='hidden lg:flex menu menu-sm menu-horizontal w-screen py-0 text-primary
                       font-semibold bg-[#F2F7F8] border-b-2 border-neutral'
     >
-      <li>
-        <NavLink to='/dashboard'>Dashboard</NavLink>
-      </li>
-      <li>
-        <NavLink to='/students'>Students</NavLink>
-      </li>
-      <li>
-        <NavLink to='/calendar'>Timetable/Calendar</NavLink>
-      </li>
-      <li>
-        <NavLink to='/learningsituations'>Learning situations</NavLink>
-      </li>
-      <li>
-        <NavLink to='/evaluation'>Evaluation</NavLink>
-      </li>
-      <li>
-        <NavLink to='/communication'>Communication</NavLink>
-      </li>
-      <li>
-        <NavLink to='/documentation'>Documentation</NavLink>
-      </li>
-      <li>
-        <NavLink to='/settings'>Settings</NavLink>
-      </li>
+      {teacherTabs}
     </ul>
   );
 };
--- src/components/MobileTeacherTabs/MobileTeacherTabs.tsx
+++ src/components/MobileTeacherTabs/MobileTeacherTabs.tsx
@@ -1,5 +1,7 @@
-import { NavLink } from 'react-router-dom';
+import { teacherTabs } from '../DesktopTeacherTabs/DesktopTeacherTabs';
+
 const MobileTeacherTabs = (): JSX.Element => {
+  const tabs = teacherTabs;
   return (
     <ul
       className='lg:hidden menu menu-md menu-horizontal w-auto py-0 mt-0 text-primary
@@ -7,38 +9,16 @@ const MobileTeacherTabs = (): JSX.Element => {
     >
       <li>
         <div className='dropdown flex justify-start items-start'>
-          <label tabIndex={0} className='btn btn-ghost'>
+          <label tabIndex={0} htmlFor='my-tabs' className='btn btn-ghost'>
             My tabs
           </label>
           <ul
             tabIndex={0}
+            id='my-tabs'
             className='menu menu-sm dropdown-content mt-2 z-[1] px-0 ml-0 bg-[#F2F7F8] 
                         rounded-box w-auto space-y-3'
           >
-            <li>
-              <NavLink to='/dashboard'>Dashboard</NavLink>
-            </li>
-            <li>
-              <NavLink to='/students'>Students</NavLink>
-            </li>
-            <li>
-              <NavLink to='/calendar'>Timetable/Calendar</NavLink>
-            </li>
-            <li>
-              <NavLink to='/learningsituations'>Learning situations</NavLink>
-            </li>
-            <li>
-              <NavLink to='/evaluation'>Evaluation</NavLink>
-            </li>
-            <li>
-              <NavLink to='/communication'>Communication</NavLink>
-            </li>
-            <li>
-              <NavLink to='/documentation'>Documentation</NavLink>
-            </li>
-            <li>
-              <NavLink to='/settings'>Settings</NavLink>
-            </li>
+            {tabs}
           </ul>
         </div>
       </li>
